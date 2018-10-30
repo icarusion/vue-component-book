24b06ad994538c2094bb50bcff918a79351c3e32
梁灏 <admin@aresn.com>
2018-10-30 10:28:29 +0800

    update

--- src/components/form/form-item.vue
+++ src/components/form/form-item.vue
@@ -9,7 +9,7 @@
 </template>
 <script>
     import AsyncValidator from 'async-validator';
-    import Emitter from '../../mixins/emitter';
+    import Emitter from '../../mixins/emitter.js';
 
     export default {
         name: 'iFormItem',
@@ -26,12 +26,13 @@
         },
         data () {
             return {
-                isRequired: false,
-                validateState: '',
-                validateMessage: '',
+                isRequired: false,  // 是否为必填
+                validateState: '',  // 校验状态
+                validateMessage: '',  // 校验不通过时的提示信息
             }
         },
         computed: {
+            // 从 Form 的 model 中动态得到当前表单组件的数据
             fieldValue () {
                 return this.form.model[this.prop];
             }
@@ -41,15 +42,15 @@
                 let rules = this.getRules();
                 if (rules.length) {
                     rules.every((rule) => {
+                        // 如果当前校验规则中有必填项，则标记出来
                         this.isRequired = rule.required;
                     });
                 }
 
-                this.$off('on-form-blur', this.onFieldBlur);
-                this.$off('on-form-change', this.onFieldChange);
                 this.$on('on-form-blur', this.onFieldBlur);
                 this.$on('on-form-change', this.onFieldChange);
             },
+            // 从 Form 的 rules 属性中，获取当前 FormItem 的校验规则
             getRules () {
                 let formRules = this.form.rules;
 
@@ -57,12 +58,16 @@
 
                 return [].concat(formRules || []);
             },
+            // 只支持 blur 和 change，所以过滤出符合要求的 rule 规则
             getFilteredRule (trigger) {
                 const rules = this.getRules();
-
-                // 只支持 blur 和 change，所以过滤出符合要求的 rule 规则
                 return rules.filter(rule => !rule.trigger || rule.trigger.indexOf(trigger) !== -1);
             },
+            /**
+             * 校验数据
+             * @param trigger 校验类型
+             * @param callback 回调函数
+             */
             validate(trigger, callback = function () {}) {
                 let rules = this.getFilteredRule(trigger);
 
@@ -70,8 +75,10 @@
                     return true;
                 }
 
+                // 设置状态为校验中
                 this.validateState = 'validating';
 
+                // 以下为 async-validator 库的调用方法
                 let descriptor = {};
                 descriptor[this.prop] = rules;
 
@@ -87,6 +94,7 @@
                     callback(this.validateMessage);
                 });
             },
+            // 重置数据
             resetField () {
                 this.validateState = '';
                 this.validateMessage = '';
@@ -100,7 +108,9 @@
                 this.validate('change');
             }
         },
+        // 组件渲染时，将实例缓存在 Form 中
         mounted () {
+            // 如果没有传入 prop，则无需校验，也就无需缓存
             if (this.prop) {
                 this.dispatch('iForm', 'on-form-item-add', this);
 
@@ -110,6 +120,7 @@
                 this.setRules();
             }
         },
+        // 组件销毁前，将实例从 Form 的缓存中移除
         beforeDestroy () {
             this.dispatch('iForm', 'on-form-item-remove', this);
         }
