6dd6e12dff9af5a4d646f069582969af6959768a
梁灏 <admin@aresn.com>
2018-10-31 17:59:46 +0800

    update

--- src/utils/assist.js
+++ src/utils/assist.js
@@ -10,3 +10,60 @@ function findComponentUpward (context, componentName) {
     return parent;
 }
 export { findComponentUpward };
+
+// 由一个组件，向上找到所有的指定组件
+function findComponentsUpward (context, componentName) {
+    let parents = [];
+    const parent = context.$parent;
+
+    if (parent) {
+        if (parent.$options.name === componentName) parents.push(parent);
+        return parents.concat(findComponentsUpward(parent, componentName));
+    } else {
+        return [];
+    }
+}
+export { findComponentsUpward };
+
+// 由一个组件，向下找到最近的指定组件
+function findComponentDownward (context, componentName) {
+    const childrens = context.$children;
+    let children = null;
+
+    if (childrens.length) {
+        for (const child of childrens) {
+            const name = child.$options.name;
+
+            if (name === componentName) {
+                children = child;
+                break;
+            } else {
+                children = findComponentDownward(child, componentName);
+                if (children) break;
+            }
+        }
+    }
+    return children;
+}
+export { findComponentDownward };
+
+// 由一个组件，向下找到所有指定的组件
+function findComponentsDownward (context, componentName) {
+    return context.$children.reduce((components, child) => {
+        if (child.$options.name === componentName) components.push(child);
+        const foundChilds = findComponentsDownward(child, componentName);
+        return components.concat(foundChilds);
+    }, []);
+}
+export { findComponentsDownward };
+
+// 由一个组件，找到指定组件的兄弟组件
+function findBrothersComponents (context, componentName, exceptMe = true) {
+    let res = context.$parent.$children.filter(item => {
+        return item.$options.name === componentName;
+    });
+    let index = res.findIndex(item => item._uid === context._uid);
+    if (exceptMe) res.splice(index, 1);
+    return res;
+}
+export { findBrothersComponents };
