56550d4ecd04a013e94bfe334fee205edc36ea66
梁灏 <admin@aresn.com>
2018-11-23 16:11:37 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,13 @@
         <div id="nav">
             <ul>
                 <li>
-                    <router-link to="/form">具有数据校验功能的表单组件 —— Form</router-link>
+                    <router-link to="/form">实战 1：具有数据校验功能的表单组件 —— Form</router-link>
                 </li>
                 <li>
-                    <router-link to="/checkbox">组合多选框组件 —— CheckboxGroup & Checkbox</router-link>
+                    <router-link to="/checkbox">实战 2：组合多选框组件 —— CheckboxGroup & Checkbox</router-link>
                 </li>
                 <li>
-                    <router-link to="/display">动态渲染 .vue 文件的组件 —— Display</router-link>
+                    <router-link to="/display">实战 3：动态渲染 .vue 文件的组件 —— Display</router-link>
                 </li>
                 <li>
                     <router-link to="/alert">实战 4：全局通知组件 —— $Alert</router-link>
